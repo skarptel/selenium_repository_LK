9c3e9d675df9aea474a034847f18caba4e45c350
lk@dsx.uk <lk@dsx.uk>
2017-05-25 20:47:27 +0300

    new test "loginLitecart"

--- softwaretesting/src/test/java/softwaretesting/Tests.java
+++ softwaretesting/src/test/java/softwaretesting/Tests.java
@@ -1,18 +1,43 @@
 package softwaretesting;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tests {
     public static final String URL = "http://software-testing.ru";
+    public static WebDriver driver = null;
+
+    @Before
+    public void settings() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/lk/selenium_repository_LK/drivers/chromedriver_win32_2.28/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void closeBrowser() {
+        //driver.close();
+        //driver.quit();
+    }
 
     @Test
     public void openSite() {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "C:/Users/lk/selenium_repository_LK/drivers/chromedriver_win32_2.28/chromedriver.exe");
-        driver = new ChromeDriver();
         driver.get(URL);
-        driver.close();
+    }
+
+    @Test
+    public void loginLitecart() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.xpath(".//*[@id='box-login']/form/div[1]/table/tbody/tr[1]/td[2]/span/input")).sendKeys("admin");
+        driver.findElement(By.xpath(".//*[@id='box-login']/form/div[1]/table/tbody/tr[2]/td[2]/span/input")).sendKeys("admin");
+        driver.findElement(By.xpath(".//*[@id='box-login']/form/div[2]/button")).click();
+        driver.findElement(By.xpath(".//*[@id='sidebar']/div[3]/ul/li[4]")).click();
+        driver.findElement(By.xpath(".//*[@id='content']/form/p/span/button[1]")).click();
+        driver.findElement(By.xpath(".//*[@id='sidebar']/div[2]/a[5]/i")).click();
     }
 }
+
+
